395af733cd18f991701c5c93d8d9a60b9b12ec59
jpclaeys <jpclaeys@gmail.com>
2019-11-27 07:26:14 +0100

    yml files

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,7 +2,7 @@
 cd $HOME
 BACKUPTYPE=daily
 
-SHORTFILESLIST='.bash_aliases .bash_profile .bashrc jpscreen.ksh .jpscreenrc_* .screenrc .gitrc .gitconfig .vimrc .ssh bin fedora_setup wiki doc tpl .config/google-chrome/Profile*/Bookmarks bak/Bookmarks_opdt Desktop/*.desktop Pictures/*.*g'
+SHORTFILESLIST='.bash_aliases .bash_profile .bashrc jpscreen.ksh .jpscreenrc_* .screenrc .gitrc .gitconfig .vimrc .ssh /home/claeyje/git/workstation_setup/bin fedora_setup wiki /home/claeyje/git/workstation_setup/doc /home/claeyje/git/workstation_setup/tpl .config/google-chrome/Profile*/Bookmarks bak/Bookmarks_opdt Desktop/*.desktop Pictures/*.*g'
 FULLFILESLIST="$SHORTFILESLIST doc_Oracle doc_IBM doc_RedHat Packages Documents log"
 
 ME=`basename $0 .sh`
@@ -30,7 +30,7 @@ BACKUPFILE=${BACKUPDIR}/${BACKUPNAME}_${BACKUPTYPE}_${TIMESTAMP}.tgz
 tar -zcf ${BACKUPFILE} ${FILESLIST} --exclude='*.iso'
 
 # Tidy up
-find ${BACKUPDIR} -iname "*daily*.tar.gz"   -ctime +7   -exec rm {} \;
-find ${BACKUPDIR} -iname "*weekly*.tar.gz"  -ctime +30  -exec rm {} \;
-find ${BACKUPDIR} -iname "*monthly*.tar.gz" -ctime +365 -exec rm {} \;
+find ${BACKUPDIR} -iname "*daily*.tgz"   -ctime +7   -exec rm {} \;
+find ${BACKUPDIR} -iname "*weekly*.tgz"  -ctime +30  -exec rm {} \;
+find ${BACKUPDIR} -iname "*monthly*.tgz" -ctime +365 -exec rm {} \;
 
